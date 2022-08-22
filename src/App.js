1463906eb6e8acb166fc8777e0d49e0270efe203
sina mohamadi <sina.m925@gmail.com>
2022-08-22 11:13:20 +0430

    Aded files of project

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import Expenses from "./components/Expenses";
+
+function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  return (
+    <div className="App h-screen w-screen">
+      <div className="text-white h-full w-full bg-[#4b4b4b] py-5">
+        <Expenses items={expenses} />
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
